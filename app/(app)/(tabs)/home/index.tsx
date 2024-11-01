8f63ceba4079f81ac8229c6321be372fa807488a
Abdennour Tadjer <abdennour.tadjer@gmail.com>
2024-11-01 19:14:39 +0100

    display some events

--- app/(app)/(tabs)/home/index.tsx
+++ app/(app)/(tabs)/home/index.tsx
@@ -5,7 +5,11 @@ import Categories from "@/components/Home/Categories";
 import Header from "@/components/Home/Header";
 import SpecialEvents from "@/components/Home/SpecialEvents";
 import PopulaireEvents from "@/components/Home/PopulaireEvents";
-import { Text } from "react-native-paper";
+import { ActivityIndicator, Text } from "react-native-paper";
+import { useQuery } from "@tanstack/react-query";
+import { getEvents } from "@/services/api/events";
+import { ThemedText } from "@/components/ThemedText";
+import { Link } from "expo-router";
 
 export default function HomeScreen() {
   const [refreshing, setRefreshing] = React.useState(false);
@@ -16,6 +20,11 @@ export default function HomeScreen() {
     }, 2000);
   }, []);
 
+  const { data, isPending, isSuccess } = useQuery({
+    queryFn: getEvents,
+    queryKey: ["events"],
+  });
+
   return (
     <SafeAreaView>
       <ScrollView
@@ -25,6 +34,22 @@ export default function HomeScreen() {
       >
         <Header />
         <Categories />
+
+        {isPending ? (
+          <ActivityIndicator animating={true} />
+        ) : isSuccess ? (
+          <ScrollView className="flex gap-4">
+            {data.data.map((event) => (
+              <View key={event.slug}>
+                <Link href={`/events/${event.slug}`} className="text-red-200">
+                  Voir event {event.title}
+                </Link>
+              </View>
+            ))}
+          </ScrollView>
+        ) : (
+          <ThemedText>something went wrong</ThemedText>
+        )}
         {/* <SpecialEvents /> */}
         {/* <PopulaireEvents /> */}
       </ScrollView>
